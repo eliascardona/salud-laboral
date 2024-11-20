a6dc8e67d36d801c67b07d0e8ab455be9e11d55a
Elias Cardona <eliascardonarod@gmail.com>
2024-11-19 18:54:52 -0600

    19-NOV-2024.   commit posterior a PULL REQUEST

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux'
 import { configuratedStore } from './redux/store/configuratedStore'
 import Page from './app/(layerOne)/page'
 
+// commit despues de un merge en origin main
+// fecha del merge:  19 de Nov. de 2024
+
 createRoot(document.getElementById('root'))
 .render(
 	<Provider store={configuratedStore}>
